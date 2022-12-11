2912cdb96f95ba79ad3ad4cb4205161af21a044b
bendafit <benidanuarifitrio@gmail.com>
2022-12-12 06:37:12 +0700

    first commit

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const PORT = 5000;
 const app = express();
 
-app.use(express.static('src'));
+app.use(express.static('public'));
 
 app.get('/', (request, response) => {
-  response.sendFile(path.resolve(__dirname, 'src/index.html'));
+    response.sendFile(path.resolve(__dirname, 'public/index.html'));
 });
 
 app.listen(PORT, () => {
-  console.log(`Your app is listening on port ${PORT}`);
+    console.log(`Your app is listening on port ${PORT}`);
 });
 
 // cara 2
